cded69ba3be8c7846b3452635ef97dba924f4d91
unknown <al.developer12@gmail.com>
2025-02-27 14:28:56 +0700

    updated

--- resources/views/package/comingsoon.blade.php
+++ resources/views/package/comingsoon.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.base')
 @section('content')
 <section class="">
-  <img src="/src/images/Under Dev Merchandise.webp" alt="Under Construction" class="d-block w-100">
+  <img src="{{env('APP_URL')}}/src/images/Under Dev Merchandise.webp" alt="Under Construction" class="d-block w-100">
 </section>
 @endsection
